591e95212a1e4694b2d267e2b36d0c84a40c8730
Darsh yadav <darsh.yadav@proton.me>
2025-09-12 20:25:27 +0530

    more decluttering

--- home-manager/modules/essentials/git.nix
+++ home-manager/modules/essentials/git.nix
@@ -1,9 +1,15 @@
 { ... }:
 
 {
-    programs.git = {
-        enable = true;
-        userName = "Darsh yadav";
-        userEmail = "darsh.yadav@proton.me";
+    options = {
+	essentialsGit.enable = lib.mkEnableOption "enables git";
+    };
+
+    config = lib.mkIf config.essentialsGit.enable {
+    	programs.git = {
+	    enable = true;
+            userName = "Darsh yadav";
+            userEmail = "darsh.yadav@proton.me";
+        };
     };
 }
