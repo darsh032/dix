8b862d00c954fb068414c8091b955f19e99b92ae
Darsh yadav <darsh.yadav@proton.me>
2025-09-13 22:15:48 +0530

    locale

--- hosts/omen/configuration.nix
+++ hosts/omen/configuration.nix
@@ -3,21 +3,6 @@
       ./hardware-configuration.nix
     ];
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
@@ -63,11 +48,6 @@
     ];
   };
 
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
      neovim
      home-manager
--- system/default.nix
+++ system/default.nix
@@ -3,5 +3,6 @@
       ./core/boot.nix
       ./core/networking.nix
       ./core/timezone.nix
+      ./core/locale.nix
     ];
 }
