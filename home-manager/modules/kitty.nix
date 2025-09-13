52eea1e8137caac15ee64dc0ab618c0a75561370
Darsh yadav <darsh.yadav@proton.me>
2025-09-13 12:50:28 +0530

    modularize kitty

--- home-manager/modules/kitty.nix
+++ home-manager/modules/kitty.nix
@@ -1,11 +1,19 @@
-{ ... }:
+{ config, lib, ... }:
 
 {
-    programs.kitty = {
-	enable = true;
-
-	settings = {
- 	    confirm_os_window_close = 0;
+    options = {
+	moduleKitty.enable = lib.mkEnableOption "Enables kitty" // {
+            default = true;
 	};
+    };
+
+    config = lib.mkIf moduleKitty.enable {
+    	programs.kitty = {
+	    enable = true;
+
+	    settings = {
+ 	        confirm_os_window_close = 0;
+	    };
+        };
     };
 }
