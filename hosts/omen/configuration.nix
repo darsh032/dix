28c339337589268be3689609a2b17e815f198699
Darsh yadav <darsh.yadav@proton.me>
2025-09-13 21:09:33 +0530

    refactoring wip

--- hosts/omen/configuration.nix
+++ hosts/omen/configuration.nix
@@ -1,7 +1,7 @@
 { config, pkgs, username, ... }: {
   imports = [
       ./hardware-configuration.nix
-      ../../nixos-modules/modules.nix
+      ../../nixos-modules/default.nix
     ];
 
   boot = {
