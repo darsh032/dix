3ed3f3770675b2b92039db43654a1acdc6e410c3
Darsh yadav <darsh.yadav@proton.me>
2025-09-16 10:49:46 +0530

    rust'

--- nvf/nvf.nix
+++ nvf/nvf.nix
@@ -1,27 +1,29 @@
 { pkgs, lib, ... }: {
-    vim = {
-       theme = {
-	   enable = true;
-	   name = "catppuccin";
-	   style = "mocha";
-       };
-
-       statusline.lualine.enable = true;
-       telescope.enable = true;
-       autocomplete.nvim-cmp.enable = true;
-       lsp.enable = true;
-
-       languages = {
-           enableTreesitter = true;
-           nix.enable = true;
-       };
-
-       additionalRuntimePaths = [
-         ./nvim
-       ];
-
-       luaConfigRC.myconfig = /* lua */ ''
-      require("myconfig")
-                '';
+  vim = {
+    theme = {
+      enable = true;
+      name = "catppuccin";
+      style = "mocha";
     };
+
+    statusline.lualine.enable = true;
+    telescope.enable = true;
+    autocomplete.nvim-cmp.enable = true;
+    lsp.enable = true;
+    snacks.enable = true;
+
+    languages = {
+      enableTreesitter = true;
+      nix.enable = true;
+      rust.enable = true;
+    };
+
+    additionalRuntimePaths = [
+      ./nvim
+    ];
+
+    luaConfigRC.myconfig = /* lua */ ''
+      require("myconfig")
+      '';
+  };
 }
