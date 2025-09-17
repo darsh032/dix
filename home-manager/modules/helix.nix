b52b2ee8d520de43cbcd12f7f934b6873e4f9a4e
Darsh yadav <darsh.yadav@proton.me>
2025-09-18 01:21:19 +0530

    helix

--- home-manager/modules/helix.nix
+++ home-manager/modules/helix.nix
@@ -1,13 +1,63 @@
-{ config, lib, ... } : {
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
   options = {
     moduleHelix.enable = lib.mkEnableOption "enables starship" // {
-      default = true; 
+      default = true;
     };
   };
 
   config = lib.mkIf config.moduleHelix.enable {
+    home.packages = with pkgs; [
+      simple-completion-language-server
+      nil
+    ];
+
     programs.helix = {
       enable = true;
+
+      languages = {
+        language-server = {
+          scls = {
+            "command" = "simple-completion-language-server";
+
+            config = {
+              "max_completion_items" = 100;
+              "feature_words" = true;
+              "feature_snippets" = true;
+              "snippets_first" = true;
+              "snippets_inline_by_word_trail" = false;
+              "feature_unicode_input" = false;
+              "feature_paths" = true;
+              "featue_citations" = false;
+            };
+
+            enviornment = {
+              "LOG_FILE" = "/tmp/completion.log";
+              "NIX_LOG" = "info,simple-completion-language-server=info";
+            };
+          };
+
+          nil_ls = {
+            "command" = "nil";
+          };
+        };
+
+        language = [
+          {
+            "name" = "nix";
+            "auto-format" = true;
+            "language-servers" = [
+              "scls"
+              "nil_ls"
+            ];
+          }
+        ];
+      };
     };
   };
 }
