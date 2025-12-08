db70f00eabfdfced47f3bbf183f8facaf44a757d
Darsh yadav <darsh.yadav@proton.me>
2025-12-08 05:53:04 +0530

    I just like end-4 too much again

--- home-manager/modules/hyprland.nix
+++ home-manager/modules/hyprland.nix
@@ -77,36 +77,35 @@ in{
         };
 
         animations = {
-          enabled = "yes, please :)";
-
+          enabled = true;
+  
           bezier = [
-            "easeOutQuint,   0.23, 1,    0.32, 1"
-            "easeInOutCubic, 0.65, 0.05, 0.36, 1"
-            "linear,         0,    0,    1,    1"
-            "almostLinear,   0.5,  0.5,  0.75, 1"
-            "quick,          0.15, 0,    0.1,  1"
+            "expressiveFastSpatial, 0.42, 1.67, 0.21, 0.90"
+            "expressiveSlowSpatial, 0.39, 1.29, 0.35, 0.98"
+            "expressiveDefaultSpatial, 0.38, 1.21, 0.22, 1.00"
+            "emphasizedDecel, 0.05, 0.7, 0.1, 1"
+            "emphasizedAccel, 0.3, 0, 0.8, 0.15"
+            "standardDecel, 0, 0, 0, 1"
+            "menu_decel, 0.1, 1, 0, 1"
+            "menu_accel, 0.52, 0.03, 0.72, 0.08"
+            "stall, 1, -0.1, 0.7, 0.85"
           ];
 
           animation = [
-            "global,        1,     10,    default"
-            "border,        1,     5.39,  easeOutQuint"
-            "windows,       1,     4.79,  easeOutQuint"
-            "windowsIn,     1,     4.1,   easeOutQuint, popin 87%"
-            "windowsOut,    1,     1.49,  linear,       popin 87%"
-            "fadeIn,        1,     1.73,  almostLinear"
-            "fadeOut,       1,     1.46,  almostLinear"
-            "fade,          1,     3.03,  quick"
-            "layers,        1,     3.81,  easeOutQuint"
-            "layersIn,      1,     4,     easeOutQuint, fade"
-            "layersOut,     1,     1.5,   linear,       fade"
-            "fadeLayersIn,  1,     1.79,  almostLinear"
-            "fadeLayersOut, 1,     1.39,  almostLinear"
-            "workspaces,    1,     1.94,  easeOutQuint, slide"
-            "workspacesIn,  1,     1.21,  easeOutQuint, slide"
-            "workspacesOut, 1,     1.94,  easeOutQuint, slide"
-            "zoomFactor,    1,     7,     quick"
+            "windowsIn, 1, 3, emphasizedDecel, popin 80%"
+            "fadeIn, 1, 3, emphasizedDecel"
+            "windowsOut, 1, 2, emphasizedDecel, popin 90%"
+            "fadeOut, 1, 2, emphasizedDecel"
+            "windowsMove, 1, 3, emphasizedDecel, slide"
+            "border, 1, 10, emphasizedDecel"
+            "layersIn, 1, 2.7, emphasizedDecel, popin 93%"
+            "layersOut, 1, 2.4, menu_accel, popin 94%"
+            "fadeLayersIn, 1, 0.5, menu_decel"
+            "fadeLayersOut, 1, 2.7, stall"
+            "workspaces, 1, 7, menu_decel, slide"
+            "specialWorkspaceIn, 1, 2.8, emphasizedDecel, slidevert"
+            "specialWorkspaceOut, 1, 1.2, emphasizedAccel, slidevert"
           ];
-
         };
 
         exec-once = [
