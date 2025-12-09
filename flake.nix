b75171a99d393edad284e9d1fe5b3d07b58b1c10
Darsh yadav <darsh.yadav@proton.me>
2025-12-09 13:58:32 +0530

    works!

--- flake.nix
+++ flake.nix
@@ -1,42 +1,6 @@
 {
   description = "Nixos config flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    hyprland.url = "github:hyprwm/Hyprland";
-    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
-    wrappers.url = "github:lassulus/wrappers";
-
-    end4 = {
-      url = "github:darsh032/dots-hyprland-fork-for-some-reason";
-      flake = false;
-    };
-
-    quickshell = {
-      url = "github:quickshell-mirror/quickshell/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    caelestia-shell = {
-      url = "github:caelestia-dots/shell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    caelestia-cli = {
-      url = "github:caelestia-dots/cli";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
 
   outputs =
     {
@@ -137,4 +101,42 @@
         };
       };
     };
+  
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    hyprland.url = "github:hyprwm/Hyprland";
+    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+    wrappers.url = "github:lassulus/wrappers";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    end4 = {
+      url = "github:darsh032/dots-hyprland-fork-for-some-reason";
+      flake = false;
+    };
+
+    quickshell = {
+      url = "github:quickshell-mirror/quickshell/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    caelestia-shell = {
+      url = "github:caelestia-dots/shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    caelestia-cli = {
+      url = "github:caelestia-dots/cli";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 }
